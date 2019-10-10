a23b8ccb86d5ac9c27cbb4de96c309d48e7155f2
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-10 12:10:33 +0800

    Created basic Mobile class

---
+++ src/main/java/mobiles/Mobile.java
@@ -0,0 +1,15 @@
+package mobiles;
+
+public class Mobile {
+    private String name;
+    private String color;
+    private String brand;
+
+    public void call(String message) {
+        System.out.println(messenger()+" : " + message);
+    }
+
+    public String messenger(){
+        return "Message";
+    }
+}
