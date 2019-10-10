5be6dc131fd7008704296edc6deb61cb41f0bca5
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-10 13:54:23 +0800

    Finish story 2

--- src/main/java/mobiles/Mobile.java
+++ src/main/java/mobiles/Mobile.java
@@ -5,11 +5,39 @@ public class Mobile {
     private String color;
     private String brand;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
     public void call(String message) {
-        System.out.println(messenger()+" : " + message);
+        if(doesExceedMessageLength(message)){
+            System.out.println(String.format("%s cannot be sent", messenger()));
+        } else{
+            System.out.println(String.format("%s : %s", messenger(), message));
+        }
+    }
+
+    private boolean doesExceedMessageLength(String message) {
+        return message.length() > messageLengthLimit();
     }
 
     protected String messenger(){
         return "Message";
+    }
+
+    protected int messageLengthLimit(){
+        return 5;
+    }
+
+    public void printInformation(){
+        System.out.println(String.format("name: %s, color %s, brand %s", name, color, brand));
     }
 }
